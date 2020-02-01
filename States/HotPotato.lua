fc95f7cfe1fb64e75d9208e6d6d2faab0992aba5
Harald Wergeland <harwer009@gmail.com>
2020-02-01 11:22:03 +0100

    potat fix

--- States/HotPotato.lua
+++ States/HotPotato.lua
@@ -2,83 +2,14 @@ local state = {}
 
 local players = {}
 
-<<<<<<< HEAD
--- Place players in circle
--- Create bomb
--- Bomb ticking
--- If bomb time over, explode
--- Kill current player, aka give all other players score
--- Create smoke particles!
--- Wait a moment before returning
-
-local bombTimer = 5
-local bombPosition = { x = 0, y = 0}
-local bombIndex = 1
-=======
 local bombTime = 5
 local bombTimer = 0
->>>>>>> b3efa4b8f018b418a7cfedf192baa42652e54b9c
 local bombBlown = false
 local bombPosition = { x = width/2, y = height/2}
 local bombIndex = 1
 local bombRadius = 30
 local bombSpeed = 10
 local Explode = function()
-<<<<<<< HEAD
-    for k,p in ipairs(PlayerManager:GetPlayers()) do
-        if bombIndex ~= k then
-            -- Increase score
-        end
-    end
-end
-
--- Do this in init
-local playerRadius = 200
-for k,p in ipairs(PlayerManager:GetPlayers()) do
-    local val = (k/20)*2*3.14
-    p.x = 1280 / 2 + math.cos(val) * playerRadius
-    p.y = 720 / 2 + math.sin(val) * playerRadius
-    p.r = 0
-end
-
-state.Update = function(self, dt)
-    if bombBlown == false then
-        for k,p in ipairs(PlayerManager:GetPlayers()) do
-            if bombIndex == k then
-                if p:GetInput() or love.keyboard.isDown('d') then
-                    bombIndex = love.math.random(20)
-                end
-            end
-        end
-        bombPosition.x = bombPosition.x + (PlayerManager:GetPlayers()[bombIndex].x - bombPosition.x) * dt * bombSpeed
-        bombPosition.y = bombPosition.y + (PlayerManager:GetPlayers()[bombIndex].y - bombPosition.y) * dt * bombSpeed
-        bombTimer = bombTimer - dt
-        if bombTimer < 0 then
-            bombBlown = true
-            Explode()
-        end
-    end
-    return false
-end
-
-state.Draw = function(self)
-    for k,p in ipairs(PlayerManager:GetPlayers()) do
-        p:Draw()
-    end
-
-    if bombBlown == false then
-        love.graphics.circle("fill", bombPosition.x, bombPosition.y, bombRadius, 16)
-    else
-        love.graphics.print("BOOM", 1280/2, 720/2, 0, 1, 1, 0, 0, 0, 0)
-    end
-
-end
-
-state.OnEnter = function()
-
-end
-
-=======
 	for k,p in ipairs(PlayerManager:GetPlayers()) do
 		if bombIndex ~= k then
 			p.score = p.score + 1
@@ -147,7 +78,6 @@ state.Draw = function(self)
 
 end
 
->>>>>>> b3efa4b8f018b418a7cfedf192baa42652e54b9c
 state.OnLeave = function()
 
 end
