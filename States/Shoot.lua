3a7a21de68c29e06cd4a11c336d095b00ea0c6f4
Risgrynsgrot <kingminiman@gmail.com>
2020-02-01 21:24:21 +0100

    small fix

--- States/Shoot.lua
+++ States/Shoot.lua
@@ -44,6 +44,7 @@ state.Draw = function(self)
 end
 
 state.OnEnter = function()
+    timeToReturn = 2
     gameIsFinished = false
     for k,p in ipairs(PlayerManager:GetPlayers()) do
         targets[k] = k
