36fc5d451d21e99f6c78e0a33c6df6b3a7925d48
Harald Wergeland <harwer009@gmail.com>
2020-02-01 20:30:48 +0100

    canoe lua file rename
    renamed canoe to .lua, added canoe state to stateManager, changed back to 20 players

--- StateManager.lua
+++ StateManager.lua
@@ -13,8 +13,8 @@ stateManager.Init = function(self)
  table.insert(self.states, require("States/Tinder"))
  table.insert(self.states, require("States/Run"))
  table.insert(self.states, require("States/CongaLine"))
-
- self.currentState = love.math.random(#self.states)
+ table.insert(self.states, require("States/Canoe"))
+ self.currentState = 9--love.math.random(#self.states)
  self.currentState = #self.states
  self.states[self.currentState]:OnEnter()
 end
