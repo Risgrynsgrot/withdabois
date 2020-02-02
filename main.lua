90b3bb9c71be903803ce946b09a8a4654e1dcde9
Risgrynsgrot <kingminiman@gmail.com>
2020-02-02 13:07:17 +0100

    music!

--- main.lua
+++ main.lua
@@ -6,8 +6,12 @@ font = love.graphics.newFont("soupofjustice.ttf", 120)
 height = 720
 width = 1280
 love.graphics.setLineWidth(5)
+local timeStamp = love.timer.getTime()
 
 function love.load()
+    music = love.audio.newSource('mainSong.wav', 'static')
+    music:setLooping(true)
+    music:play()
   love.math.setRandomSeed(love.timer.getTime())
   love.graphics.setFont(font)
   joystick = love.joystick.getJoysticks()[1]
@@ -37,6 +41,7 @@ function love.load()
 end
 
 function love.update(dt)
+    music:setPitch(1 + (love.timer.getTime() - timeStamp) * 0.001)
   sm:Update(dt)
   ParticleManager:Update(dt)
 end
